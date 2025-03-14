458378c9013a8b943276606456c9eee5c4278ee3
Matthieu Chatelin <matthieu888@gmail.com>
2025-03-14 13:28:12 +0100

    Change app.js

--- app.js
+++ app.js
@@ -15,41 +15,33 @@ const ordersRouter = require("./routes/order.routes");
 const paymentsRouter = require("./routes/payment.routes");
 
 const app = express();
-
 const cors = require("cors");
 
 app.use(cookieParser());
 
-// Activer CORS
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://my-80store-frontend.vercel.app"],
-    credentials: true, // Permet l'envoi des cookies avec la requête
+    credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
   })
 );
 
-// Gérer les pré-requêtes CORS (OPTIONS)
-app.options("*", cors());
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "build")));
 
-// Routes de l'API
-app.use("/api", indexRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/products", productsRouter);
-app.use("/api/categories", categoriesRouter);
-app.use("/api/carts", cartsRouter);
-app.use("/api/orders", ordersRouter);
-app.use("/api/payments", paymentsRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
+app.use("/carts", cartsRouter);
+app.use("/orders", ordersRouter);
+app.use("/payments", paymentsRouter);
 
-// Routes Frontend
+// Si tu application est une Single Page Application, cette ligne redirige vers ton index HTML
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
