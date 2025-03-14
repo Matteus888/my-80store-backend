7510cf92a3874518251924d36f8b6dcc0cb0a381
Matthieu Chatelin <matthieu888@gmail.com>
2025-03-14 12:35:04 +0100

    Change app.js

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://my-80store-frontend.vercel.app");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://my-80store-frontend.vercel.app");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(logger("dev"));
 app.use(express.json());
