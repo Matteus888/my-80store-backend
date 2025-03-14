7bd7be8567a0c5f69eed12f4be19570e9c73b0f2
Matthieu Chatelin <matthieu888@gmail.com>
2025-03-14 09:54:35 +0100

    Change app.js & user controller cookies setup

--- app.js
+++ app.js
@@ -18,18 +18,27 @@ const app = express();
 
 const cors = require("cors");
 
+app.use(cookieParser());
+
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
