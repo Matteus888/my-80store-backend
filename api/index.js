bac412a15a16607689161544c64d459bd076dbcc
Matteus888 <matthieu888@gmail.com>
2026-01-29 11:36:52 +0100

    Fix cors issue in app.js - Attempt 5

--- api/index.js
+++ api/index.js
@@ -4,15 +4,15 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 require("dotenv").config();
-require("./config/connection");
+require("../config/connection");
 
-const indexRouter = require("./routes/index.routes");
-const usersRouter = require("./routes/users.routes");
-const productsRouter = require("./routes/products.routes");
-const categoriesRouter = require("./routes/categories.routes");
-const cartsRouter = require("./routes/cart.routes");
-const ordersRouter = require("./routes/order.routes");
-const paymentsRouter = require("./routes/payment.routes");
+const indexRouter = require("../routes/index.routes");
+const usersRouter = require("../routes/users.routes");
+const productsRouter = require("../routes/products.routes");
+const categoriesRouter = require("../routes/categories.routes");
+const cartsRouter = require("../routes/cart.routes");
+const ordersRouter = require("../routes/order.routes");
+const paymentsRouter = require("../routes/payment.routes");
 
 const app = express();
 
