65c99211e9bf08411838e0eb3ca449b887db6002
Matteus888 <matthieu888@gmail.com>
2026-01-29 20:39:58 +0100

    Attempt to fix issu cors with all new serverless version - all 7

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,26 +2,22 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export default async function authenticate(req, res, requiredRole = null) {
-  const token = req.cookies?.token || req.headers?.authorization?.split(" ")[1];
+// Middleware async pour serverless
+export async function authenticate(req, res) {
+  const token = req.cookies?.token;
+
   if (!token) {
-    res.status(401).json({ message: "No token provided, access denied." });
+    res.status(401).json({ error: "No token provided, access denied." });
     return false;
   }
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
-
-    if (requiredRole && req.user.role !== requiredRole) {
-      res.status(403).json({ message: "You do not have permission to access this resource." });
-      return false;
-    }
-
-    return true;
+    return true; // auth OK
   } catch (err) {
     console.error("Token verification failed:", err);
-    res.status(401).json({ message: "Invalid or expired token." });
+    res.status(401).json({ message: "Invalid or expired token" });
     return false;
   }
 }
