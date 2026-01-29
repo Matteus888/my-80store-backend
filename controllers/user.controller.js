df3b7edfb067c10f60ee7393ce1a1f560306110a
Matteus888 <matthieu888@gmail.com>
2026-01-29 14:03:40 +0100

    Attempt 3 to fix issu cors

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,7 +35,7 @@ const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 jour
     });
@@ -82,7 +82,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000, // 1 jour
     });
